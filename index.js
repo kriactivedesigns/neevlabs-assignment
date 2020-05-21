d1e56b7d865222837862538ab24ff7d8807281a3
ARUN MOHANAN <kriactivedesigns@gmail.com>
2020-05-21 22:30:24 +0530

    Filters added and sidemenu added

--- index.js
+++ index.js
@@ -5,36 +5,44 @@ $(document).ready(() => {
         gender: [],
         location: [],
         specialisation: [],
-        language: []
+        languages: [],
+        availability:[]
     }
     var currentData
 
+    // Getting initial ddata of doctors list
     $.getJSON('doctors.json' , (data) => {
         var count = 0;
-        doctors = data.slice(0,5).map((item) => {
+        doctors = data.slice(0,25).map((item) => {
             return item
         })
-        console.log(doctors[0])
         currentData = doctors
         populateDoctors(doctors)        
     })
 
+    // Method to clear the filters
     function clearFilter() {
-        filter = {
+        filter = new Object({
             name: [],
             gender: [],
             location: [],
             specialisation: [],
-            language: []
-        }
+            languages: [],
+            availability:[]
+        })
         populateDoctors()
     }
 
-    const filterDataContainer = $('div#filter-data')
+    // Method to add filters to the list
     function addFilterDataToList(){
         var filterFragment = document.createDocumentFragment()
         $.map(filter, (element) => {
             element.forEach(value => {
+                if(value == 'true'){
+                    value = "Available"
+                }else if(value == 'false'){
+                    value = "Not Available"
+                }
                 var filterTemplate = document.querySelector("div.filter-item[data-type='template']").cloneNode(true)
                 $(filterTemplate).find('span.filter-value').text(value)
                 filterTemplate.style.display = "block"
@@ -54,8 +62,8 @@ $(document).ready(() => {
         delete filterFragment
     }
 
-    const searchbox = $('#search')
-    searchbox.on('input', 'input:text', (e) => {
+    // Search method
+    $('#search').on('input', 'input:text', (e) => {
         var result;
         var searchvalue = e.target.value.toUpperCase()
 
@@ -68,18 +76,101 @@ $(document).ready(() => {
         populateDoctors()
     })
 
-    const genderfilter = $('#filter-by-gender')
-    genderfilter.change((e) => {
+    // Gender filter input
+    $('.gender-dropdown-item').on('click', (e) => {
         var value = e.target.value
-        if(value === "All"){
-            clearFilter()
-        }
-        else{
-            if($.inArray(value,filter.gender) < 0){
-                filter.gender.push(value)
-            }
+        if($.inArray(value,filter.gender) < 0){
+            filter.gender.push(value)
         }
         populateDoctors()
+    })
+
+    // Specialisation filter
+    $('.specialisation-dropdown-item').on('click', (e) => {
+        var value = e.target.value
+        if($.inArray(value,filter.specialisation) < 0){
+            filter.specialisation.push(value)
+        }
+        populateDoctors()
+    })
+
+    // Availabiity filter
+    $('.availability-dropdown-item').on('click', (e) => {
+        var value = $(e.target).attr('data')
+        if($.inArray(value,filter.availability) < 0){
+            filter.availability.push(value)
+        }
+        populateDoctors()
+    })
+
+    // Location filter
+    $('.location-dropdown-item').on('click', (e) => {
+        var value = e.target.value
+        if($.inArray(value,filter.location) < 0){
+            filter.location.push(value)
+        }
+        populateDoctors()
+    })
+
+    // Language filter
+    $('.language-dropdown-item').on('click', (e) => {
+        var value = e.target.value
+        if($.inArray(value,filter.location) < 0){
+            filter.languages.push(value)
+        }
+        populateDoctors()
+    })
+
+    // Sort handler
+    $('.sort-dropdown-item').on('click', (e) => {
+        var value = e.target.value
+        var text = $(e.currentTarget).text()
+
+        $('#sort-by').text("Sort By: " + text)
+
+        switch(value){
+
+            case "alp": 
+                currentData = sortByAsc(currentData,"name")
+                populateDoctors()
+                break;
+
+            case "dhf": 
+                currentData = sortByDesc(currentData,"duration")
+                populateDoctors()
+                break;
+
+            case "fhf": 
+                currentData = sortByDesc(currentData,"fees")
+                populateDoctors()
+                break;
+
+            case "dlf": 
+                currentData = sortByAsc(currentData,"duration")
+                populateDoctors()
+                break;
+
+            case "flf": 
+                currentData = sortByAsc(currentData,"fees")
+                populateDoctors()
+                break;
+
+            default : break;
+        }
+    })
+
+    // Handle side menu drawer
+    $('.menu-button').on('click', (e) => {
+        console.log("Clicked")
+        $('.sidebar').animate({
+            left: "0"
+        }, 500)
+    })
+    $('.side-menu-close').on('click', (e) => {
+        console.log("Clicked")
+        $('.sidebar').animate({
+            left: "-350PX"
+        }, 500)
     })
 
     function filterData(data){
@@ -91,34 +182,6 @@ $(document).ready(() => {
                 isItemValid = false
                 filter.gender.forEach(gender => {
                     if(item.gender === gender){
-                        isItemValid = true
-                    }
-                });
-                if(isItemValid){
-                    return item
-                }
-            })
-        }
-
-        if(filter.name.length > 0){
-            result = $.grep(result, (item,index) => {
-                isItemValid = false
-                filter.name.forEach(name => {
-                    if(item.name === name){
-                        isItemValid = true
-                    }
-                });
-                if(isItemValid){
-                    return item
-                }
-            })
-        }
-
-        if(filter.location.length > 0){
-            result = $.grep(result, (item,index) => {
-                isItemValid = false
-                filter.location.forEach(location => {
-                    if(item.location === location){
                         isItemValid = true
                     }
                 });
@@ -142,11 +205,54 @@ $(document).ready(() => {
             })
         }
 
-        if(filter.language.length > 0){
+        if(filter.availability.length > 0){
             result = $.grep(result, (item,index) => {
                 isItemValid = false
-                filter.language.forEach(language => {
-                    if(item.language === language){
+                filter.availability.forEach(availability => {
+                    if(item.availability === JSON.parse(availability)){
+                        isItemValid = true
+                    }
+                });
+                if(isItemValid){
+                    return item
+                }
+            })
+        }
+
+        if(filter.location.length > 0){
+            result = $.grep(result, (item,index) => {
+                isItemValid = false
+                filter.location.forEach(location => {
+                    if(item.location === location){
+                        isItemValid = true
+                    }
+                });
+                if(isItemValid){
+                    return item
+                }
+            })
+        }
+
+        if(filter.languages.length > 0){
+            result = $.grep(result, (item,index) => {
+                isItemValid = false
+                filter.languages.forEach(language => {
+                    if($.inArray(language,item.languages) >= 0){
+                        console.log(item.languages)
+                        isItemValid = true
+                    }
+                });
+                if(isItemValid){
+                    return item
+                }
+            })
+        }
+
+        if(filter.name.length > 0){
+            result = $.grep(result, (item,index) => {
+                isItemValid = false
+                filter.name.forEach(name => {
+                    if(item.name === name){
                         isItemValid = true
                     }
                 });
@@ -163,7 +269,6 @@ $(document).ready(() => {
         
         addFilterDataToList()
         var filteredData = filterData(currentData)
-        console.log(filteredData)
         var keys = Object.keys(filteredData)
         var fragment = document.createDocumentFragment()
         for(var i = 0;i < keys.length; i++){
@@ -181,5 +286,21 @@ $(document).ready(() => {
         $('.doctors-list').empty()
         $('.doctors-list').append(fragment)
         delete fragment
+    }
+
+    function sortByDesc(array, key){
+        return array.sort((a,b) => {
+            var x = a[key]
+            var y = b[key]
+            return (x > y) ? -1 : ((x < y) ? 1 : 0)
+        })
+    }
+    
+    function sortByAsc(array, key){
+        return array.sort((a,b) => {
+            var x = a[key]
+            var y = b[key]
+            return (x < y) ? -1 : ((x > y) ? 1 : 0)
+        })
     }
 })
